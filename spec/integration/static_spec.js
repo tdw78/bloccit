8a5e56fe36839f5ee22d2016582789edd2668dac
Tim Weiss <tdweiss1978@gmail.com>
2019-08-29 12:53:56 -0700

    First Bloccit Assignment Complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -14,6 +15,26 @@ describe("routes : static", () => {
       });
     });
   });
+
+  describe("GET /marco", () => {
+     it("should return status code 200", (done) => {
+       
+       request.get(marco, (err, res, body) => {
+         expect(res.statusCode).toBe(200);
+
+         done();
+       });
+     });
+     it("should return the string polo", (done) => {
+
+        request.get(marco, (err, res, body) => {
+          expect(body).toBe("polo");
+
+          done();
+        });
+     });
+   });
+   
 });
 
 
