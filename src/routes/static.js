5c55ebcf42da764fdb2e1277e498c8f1ba84b4ff
Tim Weiss <tdweiss1978@gmail.com>
2019-08-30 16:01:41 -0700

    Routing Assignment Complete

--- src/routes/static.js
+++ src/routes/static.js
@@ -4,6 +4,8 @@ const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
 
+router.get("/about", staticController.about);
+
 module.exports = router;
 
 
