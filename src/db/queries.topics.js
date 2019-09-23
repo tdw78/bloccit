c611a2a5f1b38c9c232d7ca6f7b26737f701eed8
Tim Weiss <tdweiss1978@gmail.com>
2019-09-23 10:51:07 -0700

    Topic Resource 2 Complete

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -2,16 +2,65 @@ const Topic = require("./models").Topic;
 
 module.exports = {
 
-//#1
   getAllTopics(callback){
     return Topic.findAll()
 
-//#2
     .then((topics) => {
       callback(null, topics);
     })
     .catch((err) => {
       callback(err);
     })
+  },
+  addTopic(newTopic, callback){
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  getTopic(id, callback){
+    return Topic.findOne({id})
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  deleteTopic(id, callback){
+    return Topic.destroy({
+      where: {id}
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  updateTopic(id, updatedTopic, callback){
+    return Topic.findOne({id})
+    .then((topic) => {
+      if(!topic){
+        return callback("Topic not found");
+      }
+
+//#1
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+    });
   }
 }
