f8f2c6466703916a323bb72442619391e8722946
Tim Weiss <tdweiss1978@gmail.com>
2019-10-30 11:34:02 -0700

    Fixed topics

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const topicController = require("../controllers/topicController")
+const validation = require("./validation");
+const helper = require("../auth/helpers");
 
 router.get("/topics", topicController.index);
 router.get("/topics/new", topicController.new);
-router.post("/topics/create", topicController.create);
+router.post("/topics/create", 
+   helper.ensureAuthenticated,
+   validation.validatePosts,
+   topicController.create);
 router.get("/topics/:id", topicController.show);
 router.post("/topics/:id/destroy", topicController.destroy);
 router.get("/topics/:id/edit", topicController.edit);
-router.post("/topics/:id/update", topicController.update);
+router.post("/topics/:id/update", validation.validatePosts, topicController.update);
 module.exports = router;
