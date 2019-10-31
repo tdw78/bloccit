cf75f13005b83bbc2407705c4cbbbca3f54c4fd9
Tim Weiss <tdweiss1978@gmail.com>
2019-10-31 10:26:48 -0700

    Made one small change

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -36,7 +36,7 @@ module.exports = {
               };
               topicQueries.addTopic(newTopic, (err, topic) => {
                 if(err){
-                  res.redirect(500, "topics/new");
+                  res.redirect(500, "/topics/new");
                 } else {
                   res.redirect(303, `/topics/${topic.id}`);
                 }
